c12018247c1b4ac4c7dd2f12745c6993de34618a
xmyhhh <xmycissy@outlook.com>
2022-02-22 00:15:00 +0800

    变更

---
+++ problem/easy/m35.py
@@ -0,0 +1,49 @@
+"""
+35. 搜索插入位置
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+请必须使用时间复杂度为 O(log n) 的算法。
+
+
+
+示例 1:
+
+输入: nums = [1,3,5,6], target = 5
+输出: 2
+示例 2:
+
+输入: nums = [1,3,5,6], target = 2
+输出: 1
+示例 3:
+
+输入: nums = [1,3,5,6], target = 7
+输出: 4
+示例 4:
+
+输入: nums = [1,3,5,6], target = 0
+输出: 0
+示例 5:
+
+输入: nums = [1], target = 0
+输出: 0
+"""
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums)-1
+        while start <= end:
+            mid = (end - start) // 2 + start
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+        return start
+
+    def run(self):
+        print("run m35")
+        print(self.searchInsert(nums=[1, 3, 5, 6], target=0))
---
+++ problem/easy/m704.py
@@ -0,0 +1,38 @@
+"""
+704. 二分查找
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+
+示例 1:
+
+输入: nums = [-1,0,3,5,9,12], target = 9
+输出: 4
+解释: 9 出现在 nums 中并且下标为 4
+示例 2:
+
+输入: nums = [-1,0,3,5,9,12], target = 2
+输出: -1
+解释: 2 不存在 nums 中因此返回 -1
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+
+        while start <= end:
+            mid = (end - start) // 2 + start
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid+1
+            else:
+                end = mid-1
+        return -1
+
+    def run(self):
+        print("run m704")
+        print(self.search([-1, 0, 3, 5, 9, 12], target = 2))
