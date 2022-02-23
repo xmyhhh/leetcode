8a0dede041b0dc2129434aacd2901fd9366e203a
xmyhhh <xmycissy@outlook.com>
2022-02-23 22:58:44 +0800

    变更

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import problem.easy.m9
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    # Solution_level = "easy"
+    Solution_level = "easy"
 
     # Solution_level = "middle"
 
-    Solution_level = "hard"
-    Solution_num = 4
+    # Solution_level = "hard"
+    Solution_num = 237
     import_string = "from problem." + Solution_level + " import m" + str(Solution_num) + " as m"
     exec(import_string)
     solution = m.Solution() #编译器报错正常
