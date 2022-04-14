d43f0ffcb94a1d77f832ed025b5a2ad23c7634d0
xmyhhh <xmycissy@outlook.com>
2022-04-14 16:57:32 +0800

    add input helper

---
+++ nowcoder/05/05.py
@@ -0,0 +1,70 @@
+# number = list(map(int, input().split()))  # 把输入直接转成数字
+import cProfile
+# 定义测试样例class
+
+
+# 定义处理函数
+def solver(a, b, n):
+    min=0
+    detal=list(map(lambda x:abs(x[0]-x[1]),zip(a,b)))
+    a,b,detal=zip(*list(sorted(zip(a,b,detal),key=lambda x:x[2],reverse=True)))
+
+    if n<=2:
+        return max(sum(a)/2,sum(b)/2)
+    for i in range(n-1):
+        if a[i]<b[i]:
+            min=max(min,a[i]+max(a[i+1:]))
+        else:
+            min=max(min,b[i]+max(b[i+1:]))
+    return min/2
+    pass
+
+def func():
+    A_list = []
+    B_list = []
+    N = int(input())
+
+    # 处理输入生成测试样例list
+    for i in range(N):
+        number = list(map(int, input().split()))
+        A_list.append(number[0])
+        B_list.append(number[1])
+    # 逐个处理测试样例
+
+    print(solver(A_list,B_list,N))
+
+
+class myInput:
+    def __init__(self):
+        with open("input_data.txt") as file:
+            self.lines = file.readlines()
+        self.currentLine=-1
+    def getInput(self):
+        self.currentLine+=1
+        return self.lines[self.currentLine]
+
+
+def func_a():
+    input_helper=myInput()
+    n = int(input_helper.getInput())
+
+    ab = []
+    for i in range(n):
+        ab.append(list(map(int, input_helper.getInput().split())))
+    res = 0
+    ab.sort(key = lambda x:abs(x[1]-x[0]))
+    maxa = ab[0][0]
+    maxb = ab[0][1]
+    for a,b in ab:
+        if a>b:
+            res = max((b+maxb)/2,res)
+        else:
+            res = max((a+maxa)/2,res)
+        if a>maxa:
+            maxa = a
+        if b>maxb:
+            maxb = b
+    print(res)
+
+if __name__ == '__main__':
+    cProfile.run("func_a()")
